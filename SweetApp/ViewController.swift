7af5240312463d51e2e6b579657f69f7625a6862
Kevin Tabatabaei <kevintabatabaei@Kevins-MacBook-Pro.local>
2018-12-22 18:13:08 -0500

    Initial Commit

---
+++ SweetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SweetApp
+//
+//  Created by Kevin Tabatabaei on 2018-12-22.
+//  Copyright © 2018 Kevin Tabatabaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
